12ec83c2f7eb3b3debb384b9d95b9d882ec13e94
JaceLee <jaceleedev@gmail.com>
2023-03-21 10:59:13 +0900

    Bronze lv.3 2562 풀이 완료

---
+++ bronze/lv.3/2562.js
@@ -0,0 +1,19 @@
+// 백준 브론즈 3 최댓값 문제
+// https://www.acmicpc.net/problem/2562
+
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin').toString().split('\n')
+
+const arr = input.flat(1).map(Number)
+
+let max = -1
+let maxIndex = -1
+
+arr.forEach((num, index) => {
+  if (num > max) {
+    max = num
+    maxIndex = index
+  }
+})
+
+console.log(max + '\n' + (maxIndex + 1))
